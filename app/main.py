26756ca4813f1cd24754f32310d8a2c9ede75a35
Mahmoud Elhendy <mmahmoud.ali.elhendy1990@gmail.com>
2025-06-11 21:53:52 +0300

    type

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import sys
 
 def main() -> None:
     term: bool = False
+    builtin_commands: set[str] = {"echo","exit","type"}
     while not term:
         sys.stdout.write("$ ")
         # Wait for user input
@@ -11,6 +12,13 @@ def main() -> None:
             term = True
         elif command.split(' ',1)[0] == "echo":
             print(command.split(' ',1)[1])
+        elif command.split(' ',1)[0] == "type":
+            param: str = command.split(' ',1)[1]
+            if  param in builtin_commands:
+                print(f"{param} is a shell builtin")
+            else:
+                print(f"{param}: not found")
+
         else:
             print(f"{command}: command not found")
 
