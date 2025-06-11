028c7fe2d1e7d467bc495b01034db083538a73a5
Mahmoud Elhendy <mmahmoud.ali.elhendy1990@gmail.com>
2025-06-11 21:42:47 +0300

    echo

--- app/main.py
+++ app/main.py
@@ -9,6 +9,8 @@ def main() -> None:
         command: str = input()
         if command == "exit 0":
             term = True
+        elif command.split(' ',1)[0] == "echo":
+            print(command.split(' ',1)[1])
         else:
             print(f"{command}: command not found")
 
