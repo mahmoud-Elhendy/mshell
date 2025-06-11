eed19f966c5c3beda546fd2d095da3d25567549c
Mahmoud Elhendy <mmahmoud.ali.elhendy1990@gmail.com>
2025-06-12 01:51:51 +0300

    run a program

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
+import subprocess
 import sys
 import os
 
 def command_exist(command: str, dirs: list[str]) -> str | None:
+    if command == '':
+        return None
     for dir in dirs:
         try:
             files: list[str] = os.listdir(dir)
@@ -14,24 +17,32 @@ def command_exist(command: str, dirs: list[str]) -> str | None:
 def main() -> None:
     term: bool = False
     builtin_commands: set[str] = {"echo","exit","type"}
+    paths: list[str] = os.environ['PATH'].split(':')
     while not term:
         sys.stdout.write("$ ")
         # Wait for user input
         command: str = input()
+        parts: list[str] = command.split(' ', 1)
+        if len(parts) == 2:
+            prog, parmaters = parts
+        else:
+            prog= parts[0]
+            parmaters: str = ''
         if command == "exit 0":
             term = True
-        elif command.split(' ',1)[0] == "echo":
-            print(command.split(' ',1)[1])
-        elif command.split(' ',1)[0] == "type":
-            param: str = command.split(' ',1)[1]
-            paths: list[str] = os.environ['PATH'].split(':')
-            if  param in builtin_commands:
-                print(f"{param} is a shell builtin")
-            elif (path := command_exist(param , paths)) is not None:
-                print(f"{param} is {path}")
+        elif prog == "echo":
+            print(parmaters)
+        elif prog == "type":
+            if  parmaters in builtin_commands:
+                print(f"{parmaters} is a shell builtin")
+            elif (path := command_exist(parmaters , paths)) is not None:
+                print(f"{parmaters} is {path}")
             else:
-                print(f"{param}: not found")
-
+                print(f"{parmaters}: not found")
+        elif (path := command_exist(prog , paths)) is not None:
+            tokens: list[str] = [path] + parmaters.split() 
+            result: subprocess.CompletedProcess[str] = subprocess.run(tokens, capture_output=True, text=True)
+            print(result.stdout)
         else:
             print(f"{command}: command not found")
 
